3a422db17b6cdaee7eccf44525084036ff77c2f1
Olga Samsonova <medovichek@gmail.com>
2025-08-31 11:57:41 +0300

    Изменили логику doSomething () на распечатку полезного сообщения

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -10,8 +10,6 @@ public class MainApplication {
         System.out.println("---");
     }
     public static void doSomething(){
-        System.out.println(0);
-        System.out.println(1);
-        System.out.println(1);
+        System.out.println("Hello World!");
     }
 }
